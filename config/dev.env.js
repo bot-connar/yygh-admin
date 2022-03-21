d7d3df8448d7009612fe82dcb5eddd2b916b6596
zhangs41 <zhangs41@qq.com>
2022-03-21 17:15:39 +0800

    initialize yygh-admin project

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // BASE_API: '"http://localhost"',
+  BASE_API: '"http://139.198.179.93:32607"'
+})
